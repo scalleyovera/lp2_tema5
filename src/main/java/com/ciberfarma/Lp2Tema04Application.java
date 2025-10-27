bb9ca0d744bfba6d99775d6bde5c5f8f443e08d5
Danielito games <scalleyovera@gmail.com>
2025-10-26 23:09:21 -0500

    [CHG] Add project config

--- src/main/java/com/ciberfarma/Lp2Tema04Application.java
+++ src/main/java/com/ciberfarma/Lp2Tema04Application.java
@@ -2,6 +2,10 @@ package com.ciberfarma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Lp2Tema04Application {
@@ -10,4 +14,18 @@ public class Lp2Tema04Application {
 		SpringApplication.run(Lp2Tema04Application.class, args);
 	}
 
+	@Configuration
+	public static class MyConfiguration {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+				}
+			};
+		}
+	}
 }
